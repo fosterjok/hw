af983458ebefb70457b7029e88d7881ab05b54ec
fosterjok <fosterjok@mail.ru>
2022-11-04 13:44:28 +0300

    создали функцию перебирающую размер массива и подхлжящий приравнивали к новому

--- 1/Program.cs
+++ 1/Program.cs
@@ -1,2 +1,16 @@
 ﻿string [] array = new string [5] {"hello", "2", "world", "234", "23"};
 string [] array2 = new string [ array.Length];
+
+void NewArray (string[] array, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+    if(array[i].Length <= 3)
+        {
+        array2[count] = array[i];
+        count++;
+        }
+    }
+}
+NewArray(array, array2);
